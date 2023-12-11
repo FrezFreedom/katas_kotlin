9ebcd75576dc10ee890c45e092ff2505ed00c023
FrezFreedom <frez1374@gmail.com>
2023-12-11 07:58:06 +0330

    Add LengthValidatorTest and some changes

---
+++ src/test/kotlin/domain/katas/kata3/validator/LengthValidatorTest.kt
@@ -0,0 +1,46 @@
+package domain.katas.kata3.validator
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import org.katas.domain.katas.kata3.dtos.PasswordValidationResponse
+import org.katas.domain.katas.kata3.validators.LengthValidator
+
+class LengthValidatorTest {
+
+    private val lengthValidator = LengthValidator()
+
+    @ParameterizedTest
+    @MethodSource("lengthValidatorData")
+    fun testLengthValidator(expectedValue: PasswordValidationResponse, input: String){
+
+        val result = lengthValidator.validate(input)
+
+        Assertions.assertEquals(expectedValue, result)
+    }
+
+    companion object {
+        @JvmStatic
+        fun lengthValidatorData() = listOf(
+            Arguments.of(
+                PasswordValidationResponse(
+                    true,
+                    ""
+                ), "A23456789"),
+            Arguments.of(
+                PasswordValidationResponse(
+                    false,
+                    "Password must be at least 8 characters"
+                ), "C234567"),
+            Arguments.of(
+                PasswordValidationResponse(
+                    false,
+                    "Password must be at least 8 characters"
+                ), ""),
+        )
+    }
+}
---
+++ src/test/kotlin/domain/katas/kata3/validator/NumericValidatorTest.kt
@@ -0,0 +1,44 @@
+package domain.katas.kata3.validator
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import org.katas.domain.katas.kata3.dtos.PasswordValidationResponse
+import org.katas.domain.katas.kata3.validators.LengthValidator
+import org.katas.domain.katas.kata3.validators.NumericValidator
+
+class NumericValidatorTest {
+    private val numericValidator = NumericValidator()
+
+    @ParameterizedTest
+    @MethodSource("numericValidatorData")
+    fun testNumericValidator(expectedValue: PasswordValidationResponse, input: String){
+
+        val result = numericValidator.validate(input)
+
+        Assertions.assertEquals(expectedValue, result)
+    }
+
+    companion object {
+        @JvmStatic
+        fun numericValidatorData() = listOf(
+            Arguments.of(
+                PasswordValidationResponse(
+                    true,
+                    ""
+                ), "ABCDEF78"),
+            Arguments.of(
+                PasswordValidationResponse(
+                    false,
+                    "The password must contain at least 2 numbers"
+                ), "ABCDEFG8"),
+            Arguments.of(
+                PasswordValidationResponse(
+                    false,
+                    "The password must contain at least 2 numbers"
+                ), ""),
+        )
+    }
+}
