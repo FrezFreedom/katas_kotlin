186f5a4f3e661513133fdf4087c8c6627868d3db
FrezFreedom <frez1374@gmail.com>
2023-12-11 10:05:56 +0330

    Refactor CapitalValidatorTest

--- src/test/kotlin/domain/katas/kata3/validator/CapitalValidatorTest.kt
+++ src/test/kotlin/domain/katas/kata3/validator/CapitalValidatorTest.kt
@@ -3,32 +3,24 @@ package domain.katas.kata3.validator
 import io.mockk.every
 import io.mockk.mockk
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
 import org.katas.domain.katas.kata3.dtos.PasswordValidationResponse
 import org.katas.domain.katas.kata3.validator.CapitalValidator
 import org.katas.domain.katas.kata3.validator.NumberToEnglishConvertor
-import kotlin.test.BeforeTest
 
-
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class CapitalValidatorTest {
-
-    private lateinit var capitalValidator: CapitalValidator
-    private lateinit var mockNumberToEnglishConvertor: NumberToEnglishConvertor
-
-    @BeforeTest
-    fun setUp() {
-        mockNumberToEnglishConvertor = mockk<NumberToEnglishConvertor>()
-        every { mockNumberToEnglishConvertor.convert(any()) } answers { "one" }
-
-        capitalValidator = CapitalValidator(numberToEnglishConvertor = mockNumberToEnglishConvertor)
+    private val mockNumberToEnglishConvertor = mockk<NumberToEnglishConvertor> {
+        every { convert(any()) } answers { "one" }
     }
+    private val capitalValidator = CapitalValidator(numberToEnglishConvertor = mockNumberToEnglishConvertor)
 
     @ParameterizedTest
     @MethodSource("validateData")
     fun testValidate(expectedValue: PasswordValidationResponse, input: String){
-
         val result = capitalValidator.validate(input)
 
         Assertions.assertEquals(expectedValue, result)
