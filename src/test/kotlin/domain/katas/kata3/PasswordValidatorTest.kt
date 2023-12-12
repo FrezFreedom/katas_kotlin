1d4644042cd040aa7b698bc666927bde4d6c008d
FrezFreedom <frez1374@gmail.com>
2023-12-12 11:25:43 +0330

    Change Validators classes to Interface and add EndToEnd test

--- src/test/kotlin/domain/katas/kata3/PasswordValidatorTest.kt
+++ src/test/kotlin/domain/katas/kata3/PasswordValidatorTest.kt
@@ -4,21 +4,19 @@ import io.mockk.every
 import io.mockk.mockk
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
 import org.katas.domain.katas.kata3.dto.PasswordValidationResult
 import org.katas.domain.katas.kata3.PasswordValidator
-import org.katas.domain.katas.kata3.validator.CapitalValidator
-import org.katas.domain.katas.kata3.validator.LengthValidator
-import org.katas.domain.katas.kata3.validator.NumericValidator
-import org.katas.domain.katas.kata3.validator.SpecialValidator
+import org.katas.domain.katas.kata3.validator.*
 
 class PasswordValidatorTest {
-    private val lengthValidator = mockk<LengthValidator>()
-    private val numericValidator = mockk<NumericValidator>()
-    private val capitalValidator = mockk<CapitalValidator>()
-    private val specialValidator = mockk<SpecialValidator>()
+    private val lengthValidator = mockk<Validator>()
+    private val numericValidator = mockk<Validator>()
+    private val capitalValidator = mockk<Validator>()
+    private val specialValidator = mockk<Validator>()
     private lateinit var passwordValidator: PasswordValidator
 
     @DisplayName("validate should return expected result based on individual validators")
@@ -144,6 +142,19 @@ class PasswordValidatorTest {
                 PasswordValidationResult(false, mutableSetOf("g", "gg")),
             ),
         )
+    }
+
+
+    @Test
+    fun testPasswordValidatorEndToEnd() {
+        val listOfValidators = listOf(LengthValidator(), CapitalValidator(numberToEnglishConvertor = NumberToEnglishConvertor()))
+        val passwordValidator = PasswordValidator(listOfValidators)
+
+        val result = passwordValidator.validate("abcd")
+
+        Assertions.assertEquals(false, result.isValid)
+        Assertions.assertEquals(setOf("Password must be at least 8 characters", "Password must contain at least one capital letter"), result.error)
+
     }
 
 }
