04fe33a1c85359798d39cce7ce457e3e32c7f5d5
FrezFreedom <frez1374@gmail.com>
2023-12-12 08:52:42 +0330

    Update CitySearcherTest and its class ( section 2 )

--- src/main/kotlin/domain/katas/kata4/CitySearcher.kt
+++ src/main/kotlin/domain/katas/kata4/CitySearcher.kt
@@ -1,8 +1,11 @@
 package org.katas.domain.katas.kata4
 
-class CitySearcher {
+class CitySearcher(private val database: Database) {
 
     fun search(term: String): List<String> {
-        return emptyList()
+        return when {
+            term.length < 2 -> emptyList<String>()
+            else -> database.getAllCities().filter { it.startsWith(term) }
+        }
     }
 }
--- src/test/kotlin/domain/katas/kata4/CitySearcherTest.kt
+++ src/test/kotlin/domain/katas/kata4/CitySearcherTest.kt
@@ -2,19 +2,31 @@ package domain.katas.kata4
 
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import org.katas.domain.katas.kata4.CitySearcher
+import org.katas.domain.katas.kata4.Database
 
 class CitySearcherTest {
 
     @DisplayName("search should return empty list for short term")
-    @Test
-    fun testSearch(){
-        val expectedValue = emptyList<String>()
-        val citySearcher = CitySearcher()
+    @ParameterizedTest
+    @MethodSource("searchData")
+    fun testSearch(expectedValue: List<String>, term: String){
+        val database = Database()
+        val citySearcher = CitySearcher(database)
 
-        val searchResult = citySearcher.search("x")
+        val searchResult = citySearcher.search(term)
 
         Assertions.assertEquals(expectedValue, searchResult)
+    }
+
+    companion object {
+        @JvmStatic
+        fun searchData() = listOf(
+            Arguments.of(emptyList<String>(), "x"),
+            Arguments.of(listOf("Valencia", "Vancouver"), "Va"),
+        )
     }
 }
